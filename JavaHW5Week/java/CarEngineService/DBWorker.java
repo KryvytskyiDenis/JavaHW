4f169c44dbb81ab63ee47e58b311ae25e222d7d9
Denys Kryvytskyi <krivitskiy.kdn@gmail.com>
2017-02-21 18:07:10 +0200

    5 week modified

--- JavaHW5Week/java/CarEngineService/DBWorker.java
+++ JavaHW5Week/java/CarEngineService/DBWorker.java
@@ -1,6 +1,8 @@
 package CarEngineService;
 
 import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DBWorker {
     private static final String URL = "jdbc:postgresql://localhost:5432/javaHW5Week";
@@ -15,8 +17,9 @@ public class DBWorker {
     //Команды SQL
     private final String INSERT_CAR = "INSERT INTO public.car(id, model, make, id_engine, price) VALUES(?, ?, ?, ?, ?)";
     private final String INSERT_ENGINE = "INSERT INTO engine(id, displacement, power) VALUES(?, ?, ?)";
-    private final String GET_CAR_BY_ID = "SELECT * FROM car  WHERE id = " ;
+    private final String GET_CAR_BY_ID = "SELECT * FROM car WHERE id = " ;
     private final String GET_ENGINE_BY_ID = "SELECT * FROM engine WHERE id = ";
+    private final String GET_CAR_BY_ID_ENGINE = "SELECT c.id, c.model, c.make, c.price FROM car c WHERE id_engine = ";
 
     public DBWorker(){
         try {
@@ -27,32 +30,42 @@ public class DBWorker {
         }
     }
 
+    //главный метод получения авто по id и заполнение поля Engine. Возвращает объект типа Car
     public Car getCarById(int id){
-        Car car = new Car(0, "", "", null, 0);//обьект который будет содержать полученные данные
+        Car car;//обьект который будет содержать полученные данные
+
+        car = justGetCarById(id);//получаем авто по id из БД
+
+        return car;
+    }
+
+    //вспомогательный метод для получения авто. Вызывается из метода getCarById
+    private Car justGetCarById(int id){
+        Car car = new Car(0, "", "", null, 0);
 
         try{
+            //получаем Car по id
             resultSet = statement.executeQuery(GET_CAR_BY_ID + id);
 
             while(resultSet.next()){
                 car.setId(resultSet.getInt(1));
                 car.setModel(resultSet.getString(2));
                 car.setMake(resultSet.getString(3));
-                car.setEngine(getEngineById(resultSet.getInt(4)));//получаем двигатель по индексу из 4 столбика
-                car.setPrice(resultSet.getInt(4));
+                car.setEngine(getEngineById_Engine(resultSet.getInt(4)));//получаем двигатель по полученному id_engine
+                car.setPrice(resultSet.getInt(5));
             }
         } catch (SQLException e) {
             System.out.println("Error!");
         }
-
         return car;
     }
 
-    public Engine getEngineById(int id){
+    //вспомогательный метод для получения объекта типа Engine по переданному id_engine. Вызывается из метода getCarById
+    private Engine getEngineById_Engine(int id_engine){
         Engine engine = new Engine(0, 0, 0);
 
-        //используем локальный ResultSet и Statement так как вызываем этот метод в методе getCarById
         try(Statement statement = connection.createStatement()) {
-            ResultSet res = statement.executeQuery(GET_ENGINE_BY_ID + id);
+            ResultSet res = statement.executeQuery(GET_ENGINE_BY_ID + id_engine);
 
             while(res.next()){
                 engine.setId(res.getInt(1));
@@ -63,6 +76,61 @@ public class DBWorker {
             System.out.println("Error!");
         }
         return engine;
+
+    }
+
+    //главный метод получения объекта Engine по id
+    public Engine getEngineById(int id){
+        Engine engine;
+
+        engine = helpGetEngineById(id);//получаем engine по id
+
+        engine.setSetCars(getCarByIdEngine(engine.getId()));//получаем set<Car> по id_engine
+
+        return engine;
+    }
+
+    //Вспомогательный метод. Получаем объект типа Engine по id. Вызывается из метода getEngineById
+    private Engine helpGetEngineById(int id){
+        Engine engine = new Engine(0, 0, 0);
+
+        try {
+            resultSet = statement.executeQuery(GET_ENGINE_BY_ID + id);
+
+            while(resultSet.next()){
+                engine.setId(resultSet.getInt(1));
+                engine.setDisplacement(resultSet.getDouble(2));
+                engine.setPower(resultSet.getInt(3));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error!");
+        }
+        return engine;
+    }
+
+    //Вспомогательный метод. Получаем список авто по id_engine. Метод вызывается из метода getEngineById
+    private Set<Car> getCarByIdEngine(int id_engine){
+        Set<Car> setCars= new HashSet<>();
+        try{
+            //получаем Car по id_engine
+            resultSet = statement.executeQuery(GET_CAR_BY_ID_ENGINE + id_engine);
+
+            while(resultSet.next()){
+                Car car = new Car(0, "", "", null, 0);//создаем новый объект типа Car
+                //заполняем его поля даннім из БД
+                car.setId(resultSet.getInt(1));
+                car.setModel(resultSet.getString(2));
+                car.setMake(resultSet.getString(3));
+                car.setEngine(getEngineById_Engine(id_engine));
+                car.setPrice(resultSet.getInt(5));
+
+                setCars.add(car);//помещаем заполненный объект в список
+            }
+        } catch (SQLException e) {
+            System.out.println("Error!");
+        }
+
+        return setCars;
     }
 
     public void insertCar(Car car){
--- JavaHW5Week/java/CarEngineService/Main.java
+++ JavaHW5Week/java/CarEngineService/Main.java
@@ -3,38 +3,40 @@ package CarEngineService;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     private static DBWorker worker = new DBWorker();
 
-    private static List<Engine> engines = new ArrayList<>();
-
-    public static void addEngine(int id, double displacement, int power){
+    //создае новый Engine и лобавляем в БД
+    public static Engine addEngine(int id, double displacement, int power){
         Engine engine = new Engine(id, displacement, power);//Создаем объект типа Engine
         worker.insertEngine(engine);//Добавляем двигатель в БД
-        engines.add(engine);//добавляем в список двигателей
+
+        return engine;
     }
 
+    //создаем новый Car и добавляем в БД
     public static void addCar(int id, String model, String make, Engine engine, int price){
         Car car = new Car(id, model, make, engine, price);//Создаем новый объект типа Car
         engine.setCars.add(car);//добавляем авто в список авто объекта engine
-        worker.insertCar(car);//добавдяем авто в БД
+        worker.insertCar(car);//добавляем авто в БД
     }
 
     public static void main(String[] args) {
         //Добавим 3 двигателя
-        addEngine(1, 5.2, 610);
-        addEngine(2, 3.6, 400);
-        addEngine(3, 2.5, 300);
+        Engine engine1 = addEngine(1, 5.2, 610);
+        Engine engine2 =addEngine(2, 3.6, 400);
+        Engine engine3 =addEngine(3, 2.5, 300);
 
         //Добавим автомобили
-        addCar(1, "Bmw i8", "Bmw", engines.get(0), 141700);
-        addCar(2, "Bmw m5", "Bmw",  engines.get(1), 112000);
-        addCar(3, "Bmw m6", "Bmw",  engines.get(2), 70600);
-        addCar(4, "Audi a6", "Audi",  engines.get(1), 84000);
-        addCar(5, "S-Class Coupe", "Mercedes-Benz",  engines.get(2), 122750);
-        addCar(6, "Ferrari 488 GTB", "Ferrari",  engines.get(0), 249000);
-        addCar(7, "Lamborghini Huracan", "Lamborghini",  engines.get(0), 329000);
+        addCar(1, "Bmw i8", "Bmw", engine1, 141700);
+        addCar(2, "Bmw m5", "Bmw",  engine2, 112000);
+        addCar(3, "Bmw m6", "Bmw",  engine3, 70600);
+        addCar(4, "Audi a6", "Audi",  engine2, 84000);
+        addCar(5, "S-Class Coupe", "Mercedes-Benz",  engine3, 122750);
+        addCar(6, "Ferrari 488 GTB", "Ferrari",  engine1, 249000);
+        addCar(7, "Lamborghini Huracan", "Lamborghini",  engine1, 329000);
 
         //Получаем авто по id и выводим всю информацию о нем на экран
         for (int i = 1; i <= 7; i++) {
@@ -42,11 +44,17 @@ public class Main {
         }
 
         //Выводим на экран какие двигатели в каких авто используются
-        for (int i = 0; i < engines.size(); i++) {
-            System.out.println("Двигатель с индексом " + engines.get(i).getId() + " установлен в такие автомобили:");
-            System.out.println(engines.get(i).getSetCars());
-            System.out.println();
-        }
+        Set<Car> cars;
+
+        cars = engine1.getSetCars();
+        System.out.println("Двигатель с индексом " + engine1.getId() + " установлен в автомобили:\n" + cars);
+
+        cars = engine2.getSetCars();
+        System.out.println("Двигатель с индексом " + engine2.getId() + " установлен в автомобили:\n" + cars);
+
+        cars = engine3.getSetCars();
+        System.out.println("Двигатель с индексом " + engine3.getId() + " установлен в автомобили:\n" + cars);
+
 
         //Закрываем обьекты Connection, Statement, ResultSet и PreparedStatement
         worker.getResultSet();
